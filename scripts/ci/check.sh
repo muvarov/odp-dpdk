0aac4b9621b400e8dc1be6012b727a1cbb7e9dfe
Maxim Uvarov <maxim.uvarov@linaro.org>
2018-12-06 21:38:51 +0300

    Revert "travis: docker: do not mount hugepages"
    This reverts commit 37ebfb0122f8da412b62b85a25be36a674fcc3cf.

--- scripts/ci/check.sh
+++ scripts/ci/check.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+echo 1000 | tee /proc/sys/vm/nr_hugepages
+mkdir -p /mnt/huge
+mount -t hugetlbfs nodev /mnt/huge
+
 "`dirname "$0"`"/build_x86_64.sh
 
 cd "$(dirname "$0")"/../..
@@ -9,3 +13,5 @@ cd "$(dirname "$0")"/../..
 # and systems might differ in performance.
 export CI="true"
 make check
+
+umount /mnt/huge
